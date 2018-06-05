d06ef00e6db7866e91e2f244d6f4fa0ff0621a0b
sandravero <veronica_9014@hotmail.com>
2018-06-05 14:03:50 -0500

    subiendo cambios

--- src/cipher.js
+++ src/cipher.js
@@ -2,41 +2,41 @@ window.cipher = {
   encode : (keyoffset, stringAcifrar) => {
     let stringCifrado = "";
 
-    for (let i=0; i<stringAcifrar.length; i++)
+    for (let i=0; i<stringAcifrar.length; i++)//encapsula el tipo de datos de la cadena primitiva.-lengthlongitud de largo
     {
-      let numeroposicion= stringAcifrar.charCodeAt(i);
+      let numeroposicion= stringAcifrar.charCodeAt(i); // obtenemos el código ASCII
       let nuevoNumeroposicion = 0;
       if (65<=numeroposicion && numeroposicion <= 90)//mayuscula
       {
-        nuevoNumeroposicion = (numeroposicion - 65 + keyoffset) % 26 + 65;
+        nuevoNumeroposicion = (numeroposicion - 65 + keyoffset) % 26 + 65;// Obtener el nuevo código ASCII mediante fórmula y concatenamos para formar el resultado
       }
       if (97<=numeroposicion  && numeroposicion <= 122)//minuscula
       {
-        nuevoNumeroposicion = (numeroposicion - 97 + keyoffset) % 26 + 97;
+        nuevoNumeroposicion = (numeroposicion - 97 + keyoffset) % 26 + 97; // Obtener el nuevo código ASCII mediante fórmula y concatenamos para formar el resultado
       }
 
-  stringCifrado += String.fromCharCode(nuevoNumeroposicion);
+  stringCifrado += String.fromCharCode(nuevoNumeroposicion);//El String.fromCharCode() método estático que devuelve una cadena creada mediante el uso de una secuencia de valores Unicode especificada.
     }
     return stringCifrado;
   },
   decode:(keyoffset, stringCifrado) => {
     let stringDecifrado = "";
 
-    for (let i=0; i<stringCifrado.length; i++)
+    for (let i=0; i<stringCifrado.length; i++)//encapsula el tipo de datos de la cadena primitiva.-lengthlongitud de largo
     {
-      let numeroposicion = stringCifrado.charCodeAt(i);
+      let numeroposicion = stringCifrado.charCodeAt(i); // obtenemos el código ASCII
       let nuevoNumeroposicion = 0;
-      if (65<=numeroposicion && numeroposicion <= 90)
+      if (65<=numeroposicion && numeroposicion <= 90)//mayuscula
       {
-        nuevoNumeroposicion = (numeroposicion + 65 - keyoffset) % 26 + 65;
+        nuevoNumeroposicion = (numeroposicion + 65 - keyoffset) % 26 + 65;// Obtener el nuevo código ASCII mediante fórmula y concatenamos para formar el resultado
       }
-      if ( 97<= numeroposicion && numeroposicion <= 122)
+      if ( 97<= numeroposicion && numeroposicion <= 122)//minuscula
 
       {
-        nuevoNumeroposicion= (numeroposicion + 85 - keyoffset) % 26 + 97;
+        nuevoNumeroposicion= (numeroposicion + 85 - keyoffset) % 26 + 97;// Obtener el nuevo código ASCII mediante fórmula y concatenamos para formar el resultado
       }
 
-      stringDecifrado += String.fromCharCode(nuevoNumeroposicion);
+      stringDecifrado += String.fromCharCode(nuevoNumeroposicion);//El String.fromCharCode() método estático que devuelve una cadena creada mediante el uso de una secuencia de valores Unicode especificada.
     }
 
     return stringDecifrado;
