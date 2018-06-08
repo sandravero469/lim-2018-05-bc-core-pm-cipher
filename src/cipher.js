2fa0d3e3ad045deacce16869ece52647731f4a52
sandravero <veronica_9014@hotmail.com>
2018-06-08 15:50:05 -0500

    subiendo cambios

--- src/cipher.js
+++ src/cipher.js
@@ -2,24 +2,26 @@ window.cipher = {
 
 encode:(offset, string)=> {//declar el cifado para almacenar.
 
- let resultado='';// obtenemos el reciduo de la llave u offset
+  let resultado='';// obtenemos el reciduo de la llave u offset
 
- for (let i=0; i<string.length; i++){ //almacen de letras cifradas
+  for (let i=0; i<string.length; i++){ //almacen de letras cifradas
 
-   let numeroposicion = string.charCodeAt(i); // se obtiene la posicion mediante la formula.
+      let numeroposicion = string.charCodeAt(i); // se obtiene la posicion mediante la formula.
 
-  if (65 <= numeroposicion && numeroposicion <=  90)// Mayusculas
+      if (65 <= numeroposicion && numeroposicion <=  90){// Mayusculas
 
-  resultado += String.fromCharCode((numeroposicion-65+ offset) % 26 +65); // se almacena las posiciones de las letras mayusculas, y se transformamos el string.
-   else if(97 <= numeroposicion && numeroposicion <= 122)  //Minusculas
+        resultado += String.fromCharCode((numeroposicion-65+ offset) % 26 +65); // se almacena las posiciones de las letras mayusculas, y se transformamos el string.
 
-  resultado += String.fromCharCode((numeroposicion-97+ offset) % 26 +97);
-}
+      }
+   if (97 <= numeroposicion && numeroposicion <= 122)
+      {  //Minusculas
+
+        resultado += String.fromCharCode((numeroposicion-97+ offset) % 26 +97);
+          }
 // se almacena las posiciones de las letras mayusculas y concatenamos para formar el string
-
-return resultado;//mé todo estático que devuelve una cadena creada mediante el uso de una secuencia de valores Unicode especificada.
-
-},
+         }
+        return resultado;//mé todo estático que devuelve una cadena creada mediante el uso de una secuencia de valores Unicode especificada.
+        },
 
 decode:(offset, string)=> { //se declara el decifrado para almacenar
 
@@ -27,17 +29,17 @@ let resultado='';//se obtiene el residuo de la llave u offset
 
  for (let i=0; i<string.length; i++){ //encapsula letras decifradas
 
-let numeroposicion = string.charCodeAt(i); // obtenemos el código ASCIImediante la formula
-if (65 <= numeroposicion && numeroposicion <=  90)// Mayusculas
-
-resultado += String.fromCharCode((numeroposicion + 65-offset) % 26 + 65); //almacenamos las posiciones de las letras mayusculas  y concatenamos para formar el resultado
-
- else if(97 <= numeroposicion && numeroposicion <= 122)  //Minusculas
-
- resultado += String.fromCharCode(((numeroposicion - 97-offset+26) +26 )% 26 + 97);// se almacena las posiciones de las letras minusculas y concatenamos para formar el string
-}
-return resultado;//método estático que devuelve una cadena creada mediante el uso de una secuencia de valores Unicode especificada.
-},
+    let numeroposicion = string.charCodeAt(i); // obtenemos el código ASCIImediante la formula
+     if (65 <= numeroposicion && numeroposicion <=  90) {// Mayusculas
+       resultado += String.fromCharCode((numeroposicion + 65-offset) % 26 + 65); //almacenamos las posiciones de las letras mayusculas  y concatenamos para formar el resultado
+     }
+ if (97 <= numeroposicion && numeroposicion <= 122)
+        {  //Minusculas
+           resultado += String.fromCharCode(((numeroposicion - 97-offset+26) +26 )% 26 + 97);// se almacena las posiciones de las letras minusculas y concatenamos para formar el string
+             }
+            }
+            return resultado;//método estático que devuelve una cadena creada mediante el uso de una secuencia de valores Unicode especificada.
+            },
 
 
 
--- test/cipher.spec.js
+++ test/cipher.spec.js
@@ -13,42 +13,66 @@ describe('cipher', () => {
 
 
 
-    it('debería retornar "HIJKLMNOPQRSTUVWXYZABCDEFG" para "ABCDEFGHIJKLMNOPQRSTUVWXYZ" con offest 33',()=>{ let resultado = cipher.encode (33,"ABCDEFGHIJKLMNOPQRSTUVWXYZ");
-
-    assert.equal(resultado,"HIJKLMNOPQRSTUVWXYZABCDEFG");
+    it('debería retornar "HIJKLMNOPQRSTUVWXYZABCDEFG" para "ABCDEFGHIJKLMNOPQRSTUVWXYZ" con offest 33', () => {
+      let resultado = cipher.encode(33, "ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+      assert.equal(resultado, "HIJKLMNOPQRSTUVWXYZABCDEFG");
     });
   });
 
 
-  describe('cipher.decode', () => {
+  it('debería retornar "hijklmnopqrstuvwxyzabcdefg" para "abcdefghijklmnopqrstuvwxyz" con offest 33', () => {
+    let resultado = cipher.encode(33, "abcdefghijklmnopqrstuvwxyz");
+    assert.equal(resultado, "hijklmnopqrstuvwxyzabcdefg");
+  });
+});
+describe('cipher.decode', () => {
 
-    it('debería ser una función', () => {
-      assert.equal(typeof cipher.decode, "function");
-    });
+  it('debería ser una función', () => {
+    assert.equal(typeof cipher.decode, "function");
+  });
 
-    it('debería retornar "ABCDEFGHIJKLMNOPQRSTUVWXYZ" para "HIJKLMNOPQRSTUVWXYZABCDEFG" con offest 33',()=>{ let resultado = cipher.decode (33,"HIJKLMNOPQRSTUVWXYZABCDEFG");
+  it('debería retornar "ABCDEFGHIJKLMNOPQRSTUVWXYZ" para "HIJKLMNOPQRSTUVWXYZABCDEFG" con offest 33', () => {
+    let resultado = cipher.decode(33, "HIJKLMNOPQRSTUVWXYZABCDEFG");
 
-    assert.equal(resultado,"ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+    assert.equal(resultado, "ABCDEFGHIJKLMNOPQRSTUVWXYZ");
   });
 });
 
-  describe('cipher.createCipherWithOffset', () => {
+it('debería retornar "abcdefghijklmnopqrstuvwxyz" para "hijklmnopqrstuvwxyzabcdefg" con offest 33', () => {
+  let resultado = cipher.decode(33, "hijklmnopqrstuvwxyzabcdefg");
 
-    it('debería ser una función', () => {
-      assert.equal(typeof cipher.createCipherWithOffset, 'function');
-    });
-
-    it('debería retornar un objeto con dos funciones (encode y decode) con offset fijado',()=>{
-
-assert.equal(typeof cipher.createCipherWithOffset(33), "object" );
-assert.isFunction(cipher.createCipherWithOffset(33).encode);
-assert.isFunction(cipher.createCipherWithOffset(33).decode);
-   });
-it('debería retornar "HIJKLMNOPQRSTUVWXYZABCDEFG" para "ABCDEFGHIJKLMNOPQRSTUVWXYZ" con offest 33',()=> {
-  assert.equal(cipher.createCipherWithOffset(33).encode("ABCDEFGHIJKLMNOPQRSTUVWXYZ"),"HIJKLMNOPQRSTUVWXYZABCDEFG")
+  assert.equal(resultado, "abcdefghijklmnopqrstuvwxyz");
 });
-it('debería retornar "ABCDEFGHIJKLMNOPQRSTUVWXYZ" para "HIJKLMNOPQRSTUVWXYZABCDEFG" con offest 33',()=> {
-  assert.equal(cipher.createCipherWithOffset(33).decode("HIJKLMNOPQRSTUVWXYZABCDEFG"),"ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+
+describe('cipher.createCipherWithOffset', () => {
+
+  it('debería ser una función', () => {
+    assert.equal(typeof cipher.createCipherWithOffset, 'function');
+  });
+
+  it('debería retornar un objeto con dos funciones (encode y decode) con offset fijado', () => {
+
+    assert.equal(typeof cipher.createCipherWithOffset(33), "object");
+    assert.isFunction(cipher.createCipherWithOffset(33).encode);
+    assert.isFunction(cipher.createCipherWithOffset(33).decode);
+  });
+  it('debería retornar "HIJKLMNOPQRSTUVWXYZABCDEFG" para "ABCDEFGHIJKLMNOPQRSTUVWXYZ" con offest 33', () => {
+    assert.equal(cipher.createCipherWithOffset(33).encode("ABCDEFGHIJKLMNOPQRSTUVWXYZ"), "HIJKLMNOPQRSTUVWXYZABCDEFG")
+  });
+  it('debería retornar "ABCDEFGHIJKLMNOPQRSTUVWXYZ" para "HIJKLMNOPQRSTUVWXYZABCDEFG" con offest 33', () => {
+    assert.equal(cipher.createCipherWithOffset(33).decode("HIJKLMNOPQRSTUVWXYZABCDEFG"), "ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+  });
+
+it('debería retornar un objeto con dos funciones (encode y decode) con offset fijado', () => {
+
+  assert.equal(typeof cipher.createCipherWithOffset(33), "object");
+  assert.isFunction(cipher.createCipherWithOffset(33).encode);
+  assert.isFunction(cipher.createCipherWithOffset(33).decode);
 });
+it('debería retornar "hijklmnopqrstuvwxyzabcdefg" para "abcdefghijklmnopqrstuvwxyz" con offest 33', () => {
+  assert.equal(cipher.createCipherWithOffset(33).encode("abcdefghijklmnopqrstuvwxyz"), "hijklmnopqrstuvwxyzabcdefg")
+});
+it('debería retornar "ABCDEFGHIJKLMNOPQRSTUVWXYZ" para "hijklmnopqrstuvwxyzabcdefg" con offest 33', () => {
+  assert.equal(cipher.createCipherWithOffset(33).decode("hijklmnopqrstuvwxyzabcdefg"), "abcdefghijklmnopqrstuvwxyz")
 });
 });
